d109d1647268fb6300682ffd08086f7c01c02706
WARDKEY <tjrmsduq@naver.com>
2024-06-03 17:18:32 +0900

    admin 회원 목록 조회, 회원 상태 변경

--- src/main/java/com/knowget/knowgetbackend/domain/admin/controller/AdminController.java
+++ src/main/java/com/knowget/knowgetbackend/domain/admin/controller/AdminController.java
@@ -1,15 +1,56 @@
 package com.knowget.knowgetbackend.domain.admin.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.knowget.knowgetbackend.domain.admin.dto.AdminModifyDTO;
+import com.knowget.knowgetbackend.domain.admin.dto.AdminResponseDTO;
 import com.knowget.knowgetbackend.domain.admin.service.AdminService;
+import com.knowget.knowgetbackend.global.dto.ResultMessageDTO;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/admin")
 public class AdminController {
 
 	private final AdminService adminService;
+
+	/** 회원 목록 조회
+	 *
+	 * @return ResponseEntity<List < User>>
+	 * @author 근엽
+	 */
+	@GetMapping("/users")
+	public ResponseEntity<List<AdminResponseDTO>> getUsers() {
+		List<AdminResponseDTO> users = adminService.getAllUsers();
+
+		return new ResponseEntity<>(users, HttpStatus.OK);
+
+	}
+
+	/** 회원 상태 변경
+	 *
+	 * @param id
+	 * @param adminModifyDTO
+	 * @return ResponseEntity<ResultMessageDTO> 회원 상태
+	 */
+	@PatchMapping("/user/{id}")
+	public ResponseEntity<ResultMessageDTO> updateIsActive(@PathVariable("id") Integer id,
+		@RequestBody AdminModifyDTO adminModifyDTO) {
+		String message = adminService.updateIsActive(id, adminModifyDTO.getIsActive());
+
+		return new ResponseEntity<>(new ResultMessageDTO(message), HttpStatus.OK);
+
+	}
 
 }
--- src/main/java/com/knowget/knowgetbackend/domain/admin/service/AdminServiceImpl.java
+++ src/main/java/com/knowget/knowgetbackend/domain/admin/service/AdminServiceImpl.java
@@ -1,8 +1,16 @@
 package com.knowget.knowgetbackend.domain.admin.service;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.knowget.knowgetbackend.domain.admin.dto.AdminResponseDTO;
 import com.knowget.knowgetbackend.domain.admin.repository.AdminRepository;
+import com.knowget.knowgetbackend.domain.user.exception.UserNotFoundException;
+import com.knowget.knowgetbackend.domain.user.repository.UserRepository;
+import com.knowget.knowgetbackend.global.entity.User;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,5 +19,35 @@ import lombok.RequiredArgsConstructor;
 public class AdminServiceImpl implements AdminService {
 
 	private final AdminRepository adminRepository;
+	private final UserRepository userRepository;
+
+	/** 회원 목록 조회
+	 *
+	 * @return List<AdminResponseDTO>
+	 */
+	@Override
+	@Transactional(readOnly = true)
+	public List<AdminResponseDTO> getAllUsers() {
+
+		return userRepository.findAll().stream().map(AdminResponseDTO::new).collect(Collectors.toList());
+	}
+
+	/** 회원 상태 변경
+	 *
+	 * @param id
+	 * @param isActive
+	 * @return String 회원 상태
+	 */
+	@Override
+	@Transactional
+	public String updateIsActive(Integer id, Boolean isActive) {
+		User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("[ERROR] 존재하지 않는 사용자입니다."));
+		user.updateIsActive(isActive);
+		if (isActive == false) {
+			return "회원이 비활성화되었습니다.";
+		}
+		return "회원이 활성화되었습니다.";
+
+	}
 
 }
