50310c4cb606d99c2371c72771c294ac3d922620
JihwanB <leyqorwlghks@gmail.com>
2024-06-26 03:32:23 +0900

    [image] 특정 가이드에 포함된 문서 URL 반환 기능 추가

--- src/main/java/com/knowget/knowgetbackend/domain/documentTransfer/controller/DocumentTransferController.java
+++ src/main/java/com/knowget/knowgetbackend/domain/documentTransfer/controller/DocumentTransferController.java
@@ -2,7 +2,10 @@ package com.knowget.knowgetbackend.domain.documentTransfer.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +20,7 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @RestController
-@RequestMapping("/api/v1/document-transfer")
+@RequestMapping("/api/v1/document")
 @RequiredArgsConstructor
 public class DocumentTransferController {
 
@@ -42,32 +45,47 @@ public class DocumentTransferController {
 
 	/**
 	 * 문서 다중 업로드
-	 * @param jobGuideId
+	 *
+	 * @param guideId
 	 * @param files
 	 * @return imageUrls
 	 * @auther 근엽
 	 */
-	@PostMapping("{jobGuideId}/uploads")
-	public List<String> uploadFiles(@PathVariable Integer jobGuideId,
+	@PostMapping("/{guideId}/uploads")
+	public List<String> uploadFiles(@PathVariable Integer guideId,
 		@RequestParam(value = "files") List<MultipartFile> files
 	) {
 
-		List<String> documentUrls = documentTransferService.uploadFiles(files, jobGuideId);
+		List<String> documentUrls = documentTransferService.uploadFiles(files, guideId);
 
 		return documentUrls;
 	}
 
 	/**
 	 * 문서 삭제
+	 *
 	 * @param documentId
 	 * @return imageUrl
 	 * @auther 근엽
 	 */
-	@DeleteMapping("{documentId}/delete")
+	@DeleteMapping("/{documentId}/delete")
 	public String deleteDocument(@PathVariable Integer documentId) {
 		String documentUrl = documentTransferService.deleteDocument(documentId);
 
 		return documentUrl;
+	}
+
+	/**
+	 * 특정 가이드에 포함된 문서 URL 반환
+	 *
+	 * @param guideId 가이드 ID
+	 * @return imageUrls
+	 * @author Jihwan
+	 */
+	@GetMapping("/{guideId}")
+	public ResponseEntity<List<String>> getDocumentUrls(@PathVariable Integer guideId) {
+		List<String> documentUrls = documentTransferService.getDocumentUrls(guideId);
+		return new ResponseEntity<>(documentUrls, HttpStatus.OK);
 	}
 
 }
