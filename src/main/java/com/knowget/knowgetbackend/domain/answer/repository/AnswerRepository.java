b9a3cc3bbb0f3bb9b4b19ca2deccb963a9c4c291
eujin <pinklusha999@gmail.com>
2024-06-28 20:55:32 +0900

    답변 상세조회 pathVariable을 answerId에서 counselingId로 변경

--- src/main/java/com/knowget/knowgetbackend/domain/answer/repository/AnswerRepository.java
+++ src/main/java/com/knowget/knowgetbackend/domain/answer/repository/AnswerRepository.java
@@ -3,7 +3,6 @@ package com.knowget.knowgetbackend.domain.answer.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.knowget.knowgetbackend.global.entity.Answer;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
